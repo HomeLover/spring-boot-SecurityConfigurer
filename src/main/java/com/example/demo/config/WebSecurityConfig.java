c1ca809bb03d76840fa76c6a5ca46f50d0ffee58
wylyonng <2458047642@qq.com>
2021-06-01 00:03:04 +0800

    添加了多用户多权限的登录获取路由的方法

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -2,42 +2,59 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+        // 不对密码进行加密
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("root").password("123").roles("ADMIN","DBA")
+                .and()
+                .withUser("admin").password("123").roles("ADMIN","USER")
+                .and()
+                .withUser("WylYounng").password("123").roles("USER");
 
-        return new InMemoryUserDetailsManager(user);
+                // 首先配置了三个用户，root用户具备ADMIN和DBC角色
+                // admin用户 具备 ADMIN 和 USER角色
+                // Wylyounng 具备 ADMIN 和 USER角色
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests() // authorizeRequests()方法开启HttpSecurity的配置
+                .antMatchers("/admin/**") //所有经过admin路由的,都需要有ADMIN权限即ADMIN角色
+                .hasRole("ADMIN")
+                .antMatchers("/user/**")
+                .access("hasAnyRole('ADMIN','USER')")
+                .antMatchers("/db/**")
+                .access("hasRole('ADMIN') and hasRole('DBC')")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+
     }
 }
